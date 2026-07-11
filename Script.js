6d708c1dc349c8ef15f16e886a16d9e4c83025c4
leonardofollesa10-stack <leonardofollesa10@gmail.com>
2026-07-12 00:35:41 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,15 @@
+let saldo = 1000;
 
+function punta(importo){
+
+    if(importo > saldo){
+        alert("❌ Non hai abbastanza KickCoin!");
+        return;
+    }
+
+    saldo = saldo - importo;
+
+    document.getElementById("saldo").innerHTML = saldo + " 🪙";
+
+    alert("✅ Hai puntato " + importo + " KickCoin!");
+}
